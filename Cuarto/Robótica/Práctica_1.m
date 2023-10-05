1e7ef7866cb7ca3d61744816670edcfcd491ad65
Juan García <46687935+Grefuwan@users.noreply.github.com>
2023-10-05 16:42:57 +0200

    Update Práctica_1.m
    Práctica 1 - 05/10/2023

--- Cuarto/Robótica/Práctica_1.m
+++ Cuarto/Robótica/Práctica_1.m
@@ -13,7 +13,7 @@ tiempo_real(k) = 0;
 referencia(k) = signal_vf_v2(tiempo_real(k), Periodo, Delay, Amplitud);
 
 mapa = [];
-punto = animatedline('Marker', '.'); % Permite que el punto vaya "moviéndose" sin tener que borrar entero el canvas
+%punto = animatedline('Marker', '.'); % Permite que el punto vaya "moviéndose" sin tener que borrar entero el canvas
 tStart = tic;
 
 
@@ -26,30 +26,37 @@ start(motor_Cabeza);	%Inicializa el motor_Cabeza
 giro_cabeza(1) = double(readRotation(motor_Cabeza));  %Inicializa el giro_cabeza(1)
 
 %---Sonar---
-Sonar = sonicSensor(mi_Robot,4);	%Asocia el puerto 4 al Sonar
-distancia(1) = double(readDistance(Sonar));	%Guarda en el array "distancia" la 
+%Sonar = sonicSensor(mi_Robot,4);	%Asocia el puerto 4 al Sonar
+%distancia(1) = double(readDistance(Sonar));	%Guarda en el array "distancia" la 
 
-while tiempo_real(k) < 4*pi
+%---Pulsador---
+pulsador = touchSensor(mi_Robot, 1);
+
+
+while (pulsador == 0)
+end
+
+while (pulsador == 1)
+end
+
+while ((tiempo_real(k) < 4*pi) && (pulsador == 0))
 	k = k+1;
  	tiempo_real(k) = toc(tStart);
   	referencia(k) = signal_vf_v2(tiempo_real(k), Periodo, Delay, Amplitud);
     
-    %------------------------------------------
-	% Aqui en el futuro:
-	%   1- Leeremos la posicion del motor
-	%   2- El controlador calculará la acción de control a partir de la referencia(i)
-	%   3- Mandaremos el comando con la acción de control al ladrillo
-	%------------------------------------------
    
 	%---Motor Cabeza----------------
  	motor_Cabeza.Speed = 10;	%Establece la velocidad de giro de motor_Cabeza
   	giro_cabeza(k) = double(readRotation(motor_Cabeza));	%Guarda en el array giro_cabeza lo que va rotando
     
 	%---Sonar----------------
-  	distancia(k) = double(readDistance(Sonar));	%Guarda en distancia(k) la distancia leida por el Sonar en cada instante k
+  	%distancia(k) = double(readDistance(Sonar));	%Guarda en distancia(k) la distancia leida por el Sonar en cada instante k
     
 	%---Dibujar gráfica----------------
-	addpoints(punto, tiempo_real(k), distancia(k));	%Añade el siguiente punto a dibujar, sin necesidad de repintar el canvas entero
-  	axis([0 4*pi -Amplitud Amplitud]);	%Establecer las dimensiones de la gráfica a dibujar. Si no se usa, la gráfica crece dinámicamente
-  	drawnow	%Va dibujando constantemente mientras dure el bucle while
+	%addpoints(punto, tiempo_real(k), distancia(k));	%Añade el siguiente punto a dibujar, sin necesidad de repintar el canvas entero
+  	%axis([0 4*pi -Amplitud Amplitud]);	%Establecer las dimensiones de la gráfica a dibujar. Si no se usa, la gráfica crece dinámicamente
+  	%drawnow	%Va dibujando constantemente mientras dure el bucle while
+
+    disp 'Salir'
+
 end
